5c529e6732fa4ae2e9ad2e50f15772704556eff6
abdulllah <almoraiky@gmail.com>
2025-06-23 22:43:25 +0300

    Initial Commit

---
+++ formavoiceTests/formavoiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  formavoiceTests.swift
+//  formavoiceTests
+//
+//  Created by ebod on 23/06/2025.
+//
+
+import Testing
+@testable import formavoice
+
+struct formavoiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
